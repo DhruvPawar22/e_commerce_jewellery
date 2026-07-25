f3d81ba03e5ee11c37098bf08cff99656996a12a
Dhruv Pawar <dhruvpawar229@gmail.com>
2026-07-25 15:49:54 +0530

    ai slop delt

--- e-commerce-jewellery/src/App.tsx
+++ e-commerce-jewellery/src/App.tsx
@@ -1,26 +1,26 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { Layout } from './components/layout/Layout'
+// import { Layout } from './components/layout/Layout'
 import { HomePage } from './pages/HomePage'
-import { ShopPage } from './pages/ShopPage'
-import { ProductDetailPage } from './pages/ProductDetailPage'
-import { CartPage } from './pages/CartPage'
-import { AboutPage } from './pages/AboutPage'
-import { ContactPage } from './pages/ContactPage'
-import { NotFoundPage } from './pages/NotFoundPage'
+// import { ShopPage } from './pages/ShopPage'
+// import { ProductDetailPage } from './pages/ProductDetailPage'
+// import { CartPage } from './pages/CartPage'
+// import { AboutPage } from './pages/AboutPage'
+// import { ContactPage } from './pages/ContactPage'
+// import { NotFoundPage } from './pages/NotFoundPage'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route element={<Layout />}>
+        {/* <Route element={<Layout />}> */}
           <Route path="/" element={<HomePage />} />
-          <Route path="/shop" element={<ShopPage />} />
+          {/* <Route path="/shop" element={<ShopPage />} />
           <Route path="/product/:id" element={<ProductDetailPage />} />
           <Route path="/cart" element={<CartPage />} />
           <Route path="/about" element={<AboutPage />} />
           <Route path="/contact" element={<ContactPage />} />
           <Route path="*" element={<NotFoundPage />} />
-        </Route>
+        </Route> */}
       </Routes>
     </BrowserRouter>
   )
